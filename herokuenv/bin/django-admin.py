8eb9dc5dea45a2ca3421ddc3fdfc1b3aeeb87b4b
matt cole <mattcole1093@gmail.com>
2016-09-05 12:32:43 -0400

    attempting to push

---
+++ herokuenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/matt/Desktop/projects/spworkspace/splashshot/herokuenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
